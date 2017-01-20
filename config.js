5218fb729716ab88f484d5137ff001ba18a61d0b
etidbury <tidbury@live.com>
2017-01-20 10:26:30 +0000

    Tested and recorded output of tweet stream

--- config.js
+++ config.js
@@ -5,6 +5,19 @@ config.CONSUMER_KEY="BTHKUjsPwniAeOrZtIzfEpHqo";
 config.CONSUMER_SECRET="bIf3cdMcd5sNXHbHCFJamBhcRda0i0salliFuof9aN5pCT32Xv";
 config.SERVER_PORT="3002";
 
+config.TWEET_TO_HASHTAGS="#trump";
+
+
+config.SYSTEM={};
+config.SYSTEM.OUTPUT_FOLDER_PERMISSIONS='0744';
+
+
+config.SYSTEM.URI={};
+config.SYSTEM.URI.OUTPUT="./out/";
+config.SYSTEM.URI.TWEETED_USERS=config.SYSTEM.URI.OUTPUT+'tweeted_users/';
+
+
+
 
 /*
  jerrybob21
--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-require('./client');
+
+
+var c=require('./config');
+var fs = require('fs');
+
+function ensureExists(path, mask, cb) {
+    if (typeof mask == 'function') { // allow the `mask` parameter to be optional
+        cb = mask;
+        mask = '0777';
+    }
+    fs.mkdir(path, mask, function(err) {
+        if (err) {
+            if (err.code == 'EEXIST') cb(null); // ignore the error if the folder already exists
+            else cb(err); // something else went wrong
+        } else cb(null); // successfully created folder
+    });
+}
+
+console.log("Setting up environment...");
+
+ensureExists(c.SYSTEM.URI.OUTPUT,c.SYSTEM.OUTPUT_FOLDER_PERMISSIONS,function(err){
+    if (err){
+        console.error("FS Error: Failed to create directory ",c.SYSTEM.URI.OUTPUT,err);
+        return;
+    }
+
+    ensureExists(c.SYSTEM.URI.TWEETED_USERS,c.SYSTEM.OUTPUT_FOLDER_PERMISSIONS,function(err){
+        if (err){
+            console.error("FS Error: Failed to create directory ",c.SYSTEM.URI.TWEETED_USERS,err);
+            return;
+        }
+
+        console.log("Setting up client...");
+        require('./client');
+
+    });
+
+
+});
